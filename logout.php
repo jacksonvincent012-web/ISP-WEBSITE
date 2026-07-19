dd548e4b5180fab18a0952fce96b95e12dadae4d
jacksonvincent012-web <jacksonvincent012@gmail.com>
2026-07-19 18:26:27 +0300

    Add logout.php - destroy session + clear remember token cookie

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'config/functions.php'; secureSessionStart(); sendSecurityHeaders();
+clearRememberToken();
+session_unset();
+session_destroy();
+header('Location: login.php?msg=' . urlencode('You have been successfully logged out.') . '&type=success');
+exit;
